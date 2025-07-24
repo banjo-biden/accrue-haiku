def34b6091ddc73e91f4b951d2d5e24fbe033ee2
banjo-biden <banjobiden00@gmail.com>
2025-07-24 03:36:59 -0400

    Add function to randomly call a haiku from the list of haiku

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,15 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addCollection("haiku", function(collectionApi) {
     return collectionApi.getFilteredByGlob("./_haiku/*.md");
     });
+  eleventyConfig.addFilter("shuffle", function(arr) {
+    let array = [...arr];
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  });
+};
   return {
     dir: {
       input: ".",
@@ -12,4 +21,3 @@ module.exports = function(eleventyConfig) {
     }
   };
   
-};
